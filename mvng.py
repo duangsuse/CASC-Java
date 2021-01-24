50e6f7111b0e13c1d26a69ca60ab6e76062fa9f2
duangsuse <fedora-opensuse@outlook.com>
2021-01-24 23:45:17 +0800

    Update mvng

--- mvng.py
+++ mvng.py
@@ -10,6 +10,9 @@ def gavs(ae, s, sep=","): cd=_cd(s); (g,a)=cd[:2]; [gav(ae, (g,_gavAName(g,aa))+
 from os import getenv, environ
 def runUnlessEnv(name, deft, op): v=getenv(name, deft); None if v==deft else op(name, v)
 opOnEach = lambda op, xs: lambda ae: [op(ae, x) for x in xs]
+opSets = lambda name: lambda ae, x: ae().set(name, x)
+opSetsChild = lambda name: lambda ae, x: ae().append(E(name, x))
+opSetsText = lambda ae, x: setattr(ae(), "text", x)
 def proj(s, deps, javac="1.8", plugins=["org.apache.maven.plugins:maven-compiler-plugin:3.8.1"], sep=","):
   root = E("project", E("modelVersion", "4.0.0")); gav(lambda: root, _cd(s))
   runUnlessEnv("packaging", "pom", lambda t,v: root.append(E(t,v)))
@@ -21,8 +24,7 @@ def proj(s, deps, javac="1.8", plugins=["org.apache.maven.plugins:maven-compiler
   plugs = listE("plugin", opOnEach(gav, map(_cd, plugins)) )
   plugs.getchildren()[0].append(E("configuration", E("source",javac), E("target",javac)) )
   for ee in deps, E("build", plugs): root.append(ee)
-  def setsText(ae, x): ae().text = x
-  runUnlessEnv("module", "", lambda t,s: root.append(listE(t, opOnEach(setsText, s.split(sep)))))
+  runUnlessEnv("module", "", lambda t,s: root.append(listE(t, opOnEach(opSetsText, s.split(sep)))))
   return root
 
 def addEnvironParse(ap, envs, helps):
@@ -31,15 +33,49 @@ def addEnvironParse(ap, envs, helps):
   for name in envs:
     if d[name] != None: environ[name] = d[name]
   return cfg
+
+def postprocessXml(root, actions):
+  for (act, idx) in actions:
+    (path,expr) = act.split("=", 1)
+    es = root.xpath(path)
+    (mode,code) = expr.split(":", 1)
+    if mode=="file": ee=etree.parse(code) # not fromstring()
+    elif mode=="eval": ee=eval(code, globals())
+    elif mode=="text": ee=E(*code.split(":"))
+    elif mode=="new": ee=E(code)
+    if idx != None: es[idx].append(ee)
+    else:
+      for e in es: e.append(ee)
+from argparse import ArgumentParser, Action
+class CollectArg(Action): # for postprocess
+  def __init__(self, option_strings, dest, copyname="", **kwargs): super(CollectArg,self).__init__(option_strings, dest, **kwargs); self._cpname=copyname
+  def __call__(self, parser, cfg, values, option_string=None):
+    d = vars(cfg)
+    getOrPut(d, self.dest, list).append((values, d[self._cpname]))
+def getOrPut(d, k, op_init):
+  v = d.get(k)
+  if v != None: return v
+  else: v1=op_init(); d[k]=v1; return v1
+
 if __name__ == "__main__":
-  from argparse import ArgumentParser
   ap = ArgumentParser("mvng", description="generate pom.xml", epilog="names are separated by ',' and first '-' in artifactId denotes group name (require , separated)")
   dm = "dependencyManagement"; names = ["packaging", "parent", "plug", "module", dm, dm+"Suffix"]
   helps = {"plug": "plugin coordinates", "module": "module names", dm: "special dependency in this tag, with default -Suffix"}
-  ap.add_argument("coord", help="project g:a:v")
-  ap.add_argument("deps", nargs="*", help="depend g:a:v[:type:scope] s")
-  ap.add_argument("-o", help="write to dir")
+  arg = ap.add_argument
+  arg("coord", help="project g:a:v")
+  arg("deps", nargs="*", help="depend g:a:v[:type:scope] s")
+  arg("-o", help="write to dir")
+  arg("-coding", default="utf-8")
+  arg("-fat", default=False, action="store_true", help="add unnecessary attributes")
+  arg("-x", action=CollectArg, copyname="xi", help="add equations (xpath=file/text/new/eval:)")
+  arg("-xi", type=int, help="adds only at selector result[i]")
   cfg = addEnvironParse(ap, names, helps)
-  pom = etree.tostring(proj(cfg.coord, cfg.deps), pretty_print=1)
-  if cfg.o == None: print(pom.decode())
-  else: __import__("pathlib2").Path(cfg.o).joinpath("pom.xml").write_bytes(pom)
+  root = proj(cfg.coord, cfg.deps)
+  postprocessXml(root, cfg.x)
+  if cfg.fat:
+    root.set("xmlns", "http://maven.apache.org/POM/4.0.0")
+    a = [(None,"http://www.w3.org/2001/XMLSchema-instance"), ("schemaLocation","http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd")]
+    for i in range(1, len(a)): (k,v)=a[i]; root.set(etree.QName(a[i-1][1],k), v)
+  pom = etree.tostring(root, pretty_print=1, encoding=cfg.coding, xml_declaration=cfg.fat)
+  if cfg.o == None: print(pom.decode(cfg.coding))
+  else: __import__("pathlib").Path(cfg.o).joinpath("pom.xml").write_bytes(pom)
